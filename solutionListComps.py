6d96169e69c2ba2ece216900ab86a4a89f69e92f
DrDave <meichled@gmail.com>
2020-08-04 16:08:14 -0400

    added lambda function and map() solutions

--- solutionListComps.py
+++ solutionListComps.py
@@ -8,6 +8,7 @@ def stringAnagram(query, dictionary):
                 queryCount[queryIndex] +=1
     return queryCount
 """
+################################################################################
 # Solution using only one FOR loop:
 def stringAnagram1Loop(query, dictionary):
     queryCount = [0]*len(query)
@@ -23,9 +24,7 @@ print( stringAnagram1Loop(query, dictionary)) #prints [3, 2, 0] w^5
 dictionary = ['listen', 'tow', 'silent', 'lisent', 'two', 'abc', 'no', 'on']
 query = ['two', 'bca', 'no', 'listen']
 print( stringAnagram1Loop(query, dictionary)) #prints [2, 1, 2, 3] w^5
-#%%######
-
-
+################################################################################
 # Solution using only No FOR loop:
 def stringAnagramNoLoops(query, dictionary):
     return [len([1 for dictionaryWord in dictionary if len(queryWord) == len(dictionaryWord) and set(queryWord) - set(dictionaryWord) == set()]) for queryWord in query]
@@ -37,4 +36,12 @@ print( stringAnagramNoLoops(query, dictionary)) #prints [3, 2, 0] w^5
 dictionary = ['listen', 'tow', 'silent', 'lisent', 'two', 'abc', 'no', 'on']
 query = ['two', 'bca', 'no', 'listen']
 print( stringAnagramNoLoops(query, dictionary)) #prints [2, 1, 2, 3] w^5
-
+################################################################################
+#%%
+# Cute solution with lambda function:
+f = lambda queryWord:[1 for dictionaryWord in dictionary if len(queryWord) == len(dictionaryWord) and set(queryWord) - set(dictionaryWord) == set()]
+[len(f(word)) for word in query]
+#%%
+#Even cuter solution with lambda and only one list comprehension
+list(map( lambda queryWord: len([1 for dictionaryWord in dictionary if len(queryWord) == len(dictionaryWord) and set(queryWord) - set(dictionaryWord) == set()]) , query))
+#%%
