9ccf0156de83c8c5212435fc3c65156e7f472cea
DrDave <meichled@gmail.com>
2020-08-04 15:41:41 -0400

    added solution with no loops

--- solutionListComps.py
+++ solutionListComps.py
@@ -23,5 +23,18 @@ print( stringAnagram1Loop(query, dictionary)) #prints [3, 2, 0] w^5
 dictionary = ['listen', 'tow', 'silent', 'lisent', 'two', 'abc', 'no', 'on']
 query = ['two', 'bca', 'no', 'listen']
 print( stringAnagram1Loop(query, dictionary)) #prints [2, 1, 2, 3] w^5
-#%%
+#%%######
+
+
 # Solution using only No FOR loop:
+def stringAnagramNoLoops(query, dictionary):
+    return [len([1 for dictionaryWord in dictionary if len(queryWord) == len(dictionaryWord) and set(queryWord) - set(dictionaryWord) == set()]) for queryWord in query]
+#%% Tests:
+dictionary = ['heater', 'cold', 'clod', 'reheat', 'docl']
+query = ['codl', 'heater', 'abcd']
+print( stringAnagramNoLoops(query, dictionary)) #prints [3, 2, 0] w^5
+#%%
+dictionary = ['listen', 'tow', 'silent', 'lisent', 'two', 'abc', 'no', 'on']
+query = ['two', 'bca', 'no', 'listen']
+print( stringAnagramNoLoops(query, dictionary)) #prints [2, 1, 2, 3] w^5
+
